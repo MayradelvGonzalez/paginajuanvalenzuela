b6f20a3e502ee9fa8a4ba689086f2fd41448ddba
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-06-06 17:27:55 -0300

    botonInfo

--- src/componentes/main/main.jsx
+++ src/componentes/main/main.jsx
@@ -1,45 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
 import fotoPerfil from '../../assets/images/fotoPerfil.jpg';
 import 'animate.css';
-import Button from 'react-bootstrap/Button';
-import Collapse from 'react-bootstrap/Collapse';
-import Embed from 'react-embed';
+// import Embed from 'react-embed';
 import "animate.css/animate.min.css";
+import Accordion from 'react-bootstrap/Accordion';
 
 function Main() {
-  const [open, setOpen] = useState(false);
   return (
     <div className='contenido'>
         <div className="perfil">
-          <img src={ fotoPerfil } alt="fotoPerfil" />
-          <Embed url='https://www.youtube.com/watch?v=soICQ3B2kEk' />
+          <img src={fotoPerfil} alt="fotoPerfil" />
+          {/* <Embed url='https://www.youtube.com/watch?v=soICQ3B2kEk' /> */}
         </div>
+      <Accordion defaultActiveKey="0">
+
         <div className="contenidotexto">
           <h2><u> Juan Valenzuela</u></h2>
           <h3>Compositor, arreglista y profesor</h3>
-          <div>
-            <Button
-              onClick={() => setOpen(!open)}
-              aria-controls="example-collapse-text"
-              aria-expanded={open}
-            >
-              Ver más
-            </Button>
-            <Collapse in={open}>
+          <Accordion.Item eventKey="0" >
+            <Accordion.Header></Accordion.Header>
+            <Accordion.Body>
               <div id="example-collapse-text">
                 <p>Egresado del <strong>Conservatorio Provincial de música</strong></p>
                 <p>Clarinetista en la Orquesta xxx</p>
                 <p>Docente de composición y lenguaje musical</p>
               </div>
-            </Collapse>
-          </div>
+            </Accordion.Body>
+          </Accordion.Item>
 
-    
         </div>
-        </div>
-        
+
+      </Accordion>
+
+
+    </div>
+
   )
 }
 
