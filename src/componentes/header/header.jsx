b84a102491aeb2a587d0895ef019817c390faa5c
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-06-13 10:40:19 -0300

    agregandoRoutes

--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -1,8 +1,7 @@
 import './header.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {  NavLink } from 'react-router-dom';
-import '../informacion/info';
+import {  NavLink } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 
 
@@ -15,9 +14,7 @@ function header() {
       <img src={ logo } alt="logo" className='logo' />
 
         <ul>
-          <li><a href="/">Inicio</a></li>
-          <li><a href="../informacion/info.jsx">Composiciones y Arreglos</a></li>
-          {/* <li><a href="../footer/footer.jsx">Contacto</a></li> */}
+<li><NavLink to='/composiciones'>Composiciones</NavLink></li>
         </ul>
         </nav>
     
