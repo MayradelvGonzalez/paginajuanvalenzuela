9097b97d65e9ab0446c407121f378f8051f10517
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-06-13 20:12:02 -0300

    composicion.jsx

--- src/componentes/composiciones/composiciones.jsx
+++ src/componentes/composiciones/composiciones.jsx
@@ -5,52 +5,52 @@ import clarinete from '../../assets/images/clarinete.jpg';
 import clarineteIns from '../../assets/images/clarineteIns.jpg';
 import director from '../../assets/images/director.avif';
 
-function Composiciones(){
-    return(
+function Composiciones() {
+    return (
         <div className="composiciones">
-   <h1>Composiciones y arreglos</h1>
-   <Carousel className='carrusel'>
-       <Carousel.Item interval={1000}>
-         <img
-           className="d-block w-100 imagenSlide"
-           src={ director }
-           alt="Info sobre la imagen"
-         />
-         <Carousel.Caption className="info">
-           <h3>Titulo imagen</h3>
-           <p>Algo de informacion relacionada con la imagen</p>
-         </Carousel.Caption>
-       </Carousel.Item>
-       <Carousel.Item interval={500}>
-         <img
-           className="d-block w-100 imagenSlide"
-           src={ clarinete }
-           alt="Inf adicional"
-         />
-         <Carousel.Caption className="info">
-           <h3>Titulo de la imagen</h3>
-           <p>Algo de informacion relacionada con la imagen</p>
-         </Carousel.Caption>
-       </Carousel.Item>
-       <Carousel.Item>
-         <img
-           className="d-block w-100 imagenSlide"
-           src={ clarineteIns }
-           alt="Info interesante"
-         />
-         <Carousel.Caption className="info">
-           <h3>Titulo de la imagen</h3>
-           <p>
-           Algo de informacion relacionada con la imagen
-           </p>
-         </Carousel.Caption>
-       </Carousel.Item>
-     </Carousel>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, tenetur.</p>
-   <p>Lorem ipsum dolor sit amet consectetur.</p>
-   <p>Lorem ipsum dolor sit amet.</p>
+            <h1>Composiciones y arreglos</h1>
+            <Carousel className='carrusel'>
+                <Carousel.Item interval={1000}>
+                    <img
+                        className="d-block w-100 imagenSlide"
+                        src={director}
+                        alt="Info sobre la imagen"
+                    />
+                    <Carousel.Caption className="info">
+                        <h3>Titulo imagen</h3>
+                        <p>Algo de informacion relacionada con la imagen</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={500}>
+                    <img
+                        className="d-block w-100 imagenSlide"
+                        src={clarinete}
+                        alt="Inf adicional"
+                    />
+                    <Carousel.Caption className="info">
+                        <h3>Titulo de la imagen</h3>
+                        <p>Algo de informacion relacionada con la imagen</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 imagenSlide"
+                        src={clarineteIns}
+                        alt="Info interesante"
+                    />
+                    <Carousel.Caption className="info">
+                        <h3>Titulo de la imagen</h3>
+                        <p>
+                            Algo de informacion relacionada con la imagen
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, tenetur.</p>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
         </div>
-     
+
     )
 }
 export default Composiciones;
